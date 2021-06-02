339691e05b0ab0d2b6ab05baa0146d522ae2f40a
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-02 17:43:26 +0200

    [feat][#4] Add new endpoint `/etl` that will enrich AND index in ES

--- bso/server/main/elastic.py
+++ bso/server/main/elastic.py
@@ -1,60 +1,47 @@
-import datetime
 import os
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Q, Search, A
-from elasticsearch import helpers
+
+from elasticsearch import Elasticsearch, helpers
+
 from bso.server.main.logger import get_logger
 
 logger = get_logger(__name__)
 
-es = None
+client = None
 try:
-    es = Elasticsearch([os.getenv("ES_URL")],http_auth=(os.getenv("ES_LOGIN"),os.getenv('ES_PASSWORD')))
+    client = Elasticsearch(os.getenv('ES_URL'), http_auth=(os.getenv('ES_LOGIN'), os.getenv('ES_PASSWORD')))
 except:
-    logger.debug("cannot connect to es")
+    logger.debug('Cannot connect to es')
 
-def delete_index(myIndex):
-    logger.debug("deleting "+myIndex)
-    del_docs = es.delete_by_query(index=myIndex, body={"query": {"match_all": {}}})
-    logger.debug(del_docs)
-    del_index = es.indices.delete(index=myIndex, ignore=[400, 404])
-    logger.debug(del_index)
-    return
 
-def reset_index(myIndex, filters={}, char_filters={}, tokenizers={}, analyzers={}):
+def delete_index(index: str):
+    logger.debug(f'Deleting {index}')
+    response = client.indices.delete(index=index, ignore=[400, 404])
+    logger.debug(response)
+
+
+def reset_index(index):
     try:
-        delete_index(myIndex)
+        delete_index(index)
     except:
-        logger.debug("delete failed")
-
-    response = es.indices.create(
-              index=myIndex,
-              body={
-                  "settings": {},
-                  "mappings": {}   
-              },
-              ignore=400 # ignore 400 already exists code
-          )
-  
+        logger.debug('Index deletion failed')
+    response = client.indices.create(
+        index=index,
+        body={'settings': {}, 'mappings': {}},
+        ignore=400  # ignore 400 already exists code
+    )
     if 'acknowledged' in response:
-        if response['acknowledged'] == True:
-            logger.debug("INDEX MAPPING SUCCESS FOR INDEX:" + str(response['index']))
- 
-def load_in_es(data_to_import, myIndex):
-    start = datetime.datetime.today()
+        if response['acknowledged']:
+            response = str(response['index'])
+            logger.debug(f'Index mapping success for index: {response}')
+
+
+def load_in_es(data: list, index: str) -> None:
     actions = [
         {
-          "_index": myIndex,
-          "_type": "_doc",
-          "_id": j,
-          "_source": data_to_import[j]
+            '_index': index,
+            '_source': data
         }
-    for j in range(0, len(data_to_import))
+        for i, data in enumerate(data)
     ]
-    bulk_res = helpers.bulk(es, actions, chunk_size=500)
-
-    end = datetime.datetime.today()
-    delta = (end - start)
-    nb = len(data_to_import)
-    logger.debug(f"{nb} elts imported into {myIndex} in {delta}")
-
+    helpers.parallel_bulk(client=client, actions=actions, chunk_size=500, request_timeout=60)
+    logger.debug(f'{len(data)} elements imported into {index}')
