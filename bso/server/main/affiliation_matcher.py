91127771aa77f431226050d9b52ca2fb28b05936
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-03 11:08:55 +0200

    [fix] Add affiliation matcher on each needed field

--- bso/server/main/affiliation_matcher.py
+++ bso/server/main/affiliation_matcher.py
@@ -6,9 +6,24 @@ AFFILIATION_MATCHER_SERVICE = os.getenv('AFFILIATION_MATCHER_SERVICE')
 
 
 def filter_publications_by_country(publications: list, country: str = 'fr') -> list:
+    field_name = 'detected_countries'
+    endpoint_url = f'{AFFILIATION_MATCHER_SERVICE}/match_api'
+    all_countries = []
     for publication in publications:
-        countries = requests.post(f'{AFFILIATION_MATCHER_SERVICE}/match_api', json={'query': publication,
-                                                                                    'type': 'country'})
-        publication['matched_countries'] = countries
-    french_publications = [publication for publication in publications if country in publication['matched_countries']]
-    return french_publications
+        affiliations = publication.get('affiliations', [])
+        for affiliation in affiliations:
+            query = affiliation.get('name')
+            countries = requests.post(endpoint_url, json={'query': query, 'type': 'country'})
+            affiliation[field_name] = countries
+            all_countries += countries
+        authors = publication.get('authors', [])
+        for author in authors:
+            affiliations = author.get('affiliations', [])
+            for affiliation in affiliations:
+                query = affiliation.get('name')
+                countries = requests.post(endpoint_url, json={'query': query, 'type': 'country'})
+                affiliation[field_name] = countries
+                all_countries += countries
+        publication[field_name] = list(set(countries))
+    filtered_publications = [publication for publication in publications if country.lower() in publication[field_name]]
+    return filtered_publications
