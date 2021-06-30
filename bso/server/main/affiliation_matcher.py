becd534088cb2206e92999779022ee347451499d
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-30 16:30:26 +0200

    [feat] Add logs to "affiliation_matcher"

--- bso/server/main/affiliation_matcher.py
+++ bso/server/main/affiliation_matcher.py
@@ -1,10 +1,15 @@
 import os
 import requests
 
+from bso.server.main.logger import get_logger
+
 AFFILIATION_MATCHER_SERVICE = os.getenv('AFFILIATION_MATCHER_SERVICE')
+
+logger = get_logger(__name__)
 
 
 def filter_publications_by_country(publications: list, countries_to_keep: list = None) -> list:
+    logger.debug(f'Filter {len(publications)} publication against {",".join(countries_to_keep)} countries.')
     if countries_to_keep is None:
         countries_to_keep = []
     field_name = 'detected_countries'
@@ -20,14 +25,17 @@ def filter_publications_by_country(publications: list, countries_to_keep: list =
             affiliations = author.get('affiliation', [])
             affiliations = [] if affiliations is None else affiliations
             all_affiliations += [affiliation.get('name') for affiliation in affiliations]
+    logger.debug(f'Found {len(all_affiliations)} affiliations in total.')
     # Deduplicate affiliations
-    all_affiliations = filter(None, list(set(all_affiliations)))
+    all_affiliations = list(filter(None, list(set(all_affiliations))))
+    logger.debug(f'Found {len(all_affiliations)} different affiliations in total.')
     # Transform list into dict
     all_affiliations = {key: None for key in all_affiliations}
     # Retrieve countries for all publications
     for affiliation in all_affiliations:
         countries = requests.post(endpoint_url, json={'query': affiliation, 'type': 'country'}).json()['results']
         all_affiliations[affiliation] = countries
+    logger.debug('All countries of all affiliations have been retrieved.')
     # Map countries with affiliations
     for publication in publications:
         countries_by_publication = []
@@ -53,4 +61,5 @@ def filter_publications_by_country(publications: list, countries_to_keep: list =
         countries_to_keep_set = set(countries_to_keep)
         filtered_publications = [publication for publication in publications
                                  if len(set(publication[field_name]).intersection(countries_to_keep_set)) > 0]
+    logger.debug(f'After filtering by countries, {len(filtered_publications)} publications have been kept.')
     return filtered_publications
