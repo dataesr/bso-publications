88762af6190cfb77fa12514beb78f70df532b199
eric <eric.jeangirard@recherche.gouv.fr>
2021-06-30 22:40:07 +0200

    [speedup] using concurrent requests for country matcher

--- bso/server/main/affiliation_matcher.py
+++ bso/server/main/affiliation_matcher.py
@@ -1,12 +1,17 @@
 import os
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 from bso.server.main.logger import get_logger
 
 AFFILIATION_MATCHER_SERVICE = os.getenv('AFFILIATION_MATCHER_SERVICE')
+NB_AFFILIATION_MATCHER = 5
 
 logger = get_logger(__name__)
 
+def get_country(affiliation):
+    countries = requests.post(endpoint_url, json={'query': affiliation, 'type': 'country'}).json()['results']
+    return countries
 
 def filter_publications_by_country(publications: list, countries_to_keep: list = None) -> list:
     logger.debug(f'Filter {len(publications)} publication against {",".join(countries_to_keep)} countries.')
@@ -27,14 +32,18 @@ def filter_publications_by_country(publications: list, countries_to_keep: list =
             all_affiliations += [affiliation.get('name') for affiliation in affiliations]
     logger.debug(f'Found {len(all_affiliations)} affiliations in total.')
     # Deduplicate affiliations
-    all_affiliations = list(filter(None, list(set(all_affiliations))))
+    all_affiliations_list = list(filter(None, list(set(all_affiliations))))
     logger.debug(f'Found {len(all_affiliations)} different affiliations in total.')
     # Transform list into dict
-    all_affiliations = {key: None for key in all_affiliations}
+    all_affiliations_dict = {key: None for key in all_affiliations_list}
     # Retrieve countries for all publications
-    for affiliation in all_affiliations:
-        countries = requests.post(endpoint_url, json={'query': affiliation, 'type': 'country'}).json()['results']
-        all_affiliations[affiliation] = countries
+    #for affiliation in all_affiliations:
+    #    countries = requests.post(endpoint_url, json={'query': affiliation, 'type': 'country'}).json()['results']
+    #    all_affiliations[affiliation] = countries
+    with ThreadPoolExecutor(max_workers=NB_AFFILIATION_MATCHER) as pool:
+        countries_list = list(pool.map(get_country,all_affiliations_list))
+    for ix, affiliation in enumerate(all_affiliations_list):
+        all_affiliations_dict[affiliation] = countries_list[ix]
     logger.debug('All countries of all affiliations have been retrieved.')
     # Map countries with affiliations
     for publication in publications:
@@ -43,7 +52,7 @@ def filter_publications_by_country(publications: list, countries_to_keep: list =
         affiliations = [] if affiliations is None else affiliations
         for affiliation in affiliations:
             query = affiliation.get('name')
-            countries = all_affiliations[query]
+            countries = all_affiliations_dict[query]
             affiliation[field_name] = countries
             countries_by_publication += countries
         authors = publication.get('authors', [])
@@ -51,7 +60,7 @@ def filter_publications_by_country(publications: list, countries_to_keep: list =
             affiliations = author.get('affiliation', [])
             for affiliation in affiliations:
                 query = affiliation.get('name')
-                countries = all_affiliations[query]
+                countries = all_affiliations_dict[query]
                 affiliation[field_name] = countries
                 countries_by_publication += countries
         publication[field_name] = list(set(countries_by_publication))
