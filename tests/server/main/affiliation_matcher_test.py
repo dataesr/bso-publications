8e0c2f1fd15c22dbf9475d576219fd4f8c0fddd4
Anne L'Hôte <anne.lhote@gmail.com>
2021-07-26 17:30:58 +0200

    [ci] Add test

--- tests/server/main/affiliation_matcher_test.py
+++ tests/server/main/affiliation_matcher_test.py
@@ -1,3 +1,11 @@
+from bso.server.main.affiliation_matcher import check_matcher_health
+from bso.server.main.config import AFFILIATION_MATCHER_SERVICE
+
+
 class TestAffiliationMatcher:
-    def test_check_matcher_health(self) -> None:
-        assert True is True
+    def test_check_matcher_health(self, requests_mock) -> None:
+        url = f'{AFFILIATION_MATCHER_SERVICE}/match_api'
+        requests_mock.post(url=url, json={'results': []})
+        result = check_matcher_health()
+        assert result is True
+
