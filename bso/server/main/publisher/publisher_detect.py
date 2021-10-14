f495f891303d8a7d8ec5d501833337e77f792912
eric <eric.jeangirard@recherche.gouv.fr>
2021-10-15 00:07:43 +0200

    publishers

--- bso/server/main/publisher/publisher_detect.py
+++ bso/server/main/publisher/publisher_detect.py
@@ -1,14 +1,22 @@
 import pandas as pd
+import re
+import unidecode
 
 df_publisher = pd.read_csv('bso/server/main/publisher/publisher_correspondance.csv')
-publisher_map = {}
-for i, row in df_publisher.iterrows():
-    raw = row.publisher_raw.strip()
-    target = row.publisher.strip()
-    publisher_map[raw] = target
 
-# normalise le nom de l'éditeur
+rgx_list = []
+for i, row in df.iterrows():
+    rgx_list.append({'pattern': re.compile(row.publisher_raw, re.IGNORECASE), 'publisher_normalized': row.publisher_clean})
 
-def detect_publisher(publisher: str) -> str:
-    publisher_normalized = publisher_map.get(publisher.strip(), publisher)   
-    return {'publisher_normalized': publisher_normalized}
+def detect_publisher(raw_input):
+    unaccented_string = unidecode.unidecode(raw_input).replace(',', ' ').replace('  ', ' ')
+    unaccented_string = unaccented_string.replace("’", "'")
+    without_parenthesis = re.sub(r'\([^)]*\)', '', unaccented_string)
+    tested_string = without_parenthesis.strip()
+    if '/' in tested_string:
+        return {'publisher_normalized': raw_input}
+    for r in rgx_list:
+        if re.search(r['pattern'], tested_string):
+            return r['publisher']
+            return {'publisher_normalized': r['publisher_normalized']}
+    return {'publisher_normalized': raw_input}
