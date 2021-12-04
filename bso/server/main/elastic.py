4163170e480d89fa73694b995857a705a7551397
eric <eric.jeangirard@recherche.gouv.fr>
2021-12-04 15:53:13 +0100

    elastic mappings

--- bso/server/main/elastic.py
+++ bso/server/main/elastic.py
@@ -136,7 +136,8 @@ def reset_index(index: str) -> None:
         dynamic_match = "*authors"
 
     mappings = { 'properties': {} }
-    for f in ['z_authors.family', 'z_authors.given', 'title', 'journal_name']:
+    for f in ['z_authors.family', 'z_authors.given', 'title', 'journal_name', 
+      'affiliations.name', 'authors.first_name', 'authors.last_name', 'authors.full_name', 'authors.affiliations.name']:
         mappings['properties'][f] = { 
                 'type': 'text',
                 'analyzer': 'light' 
