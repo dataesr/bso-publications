5101ad78314a2e59aed9a72913ea722bc21f5b05
eric <eric.jeangirard@recherche.gouv.fr>
2023-10-24 21:40:01 +0200

    testing

--- bso/server/main/extra_treatment.py
+++ bso/server/main/extra_treatment.py
@@ -1,13 +1,42 @@
 import pandas as pd
 import os
 import requests
+import pymongo
+from bso.server.main.logger import get_logger
 from bso.server.main.utils_swift import download_object, upload_object
 from bso.server.main.bso_utils import get_ror_from_local
 from bso.server.main.utils import to_jsonl
 from bso.server.main.extract_transform import load_scanr_publications
+from bso.server.main.unpaywall_feed import download_daily, download_snapshot, snapshot_to_mongo, load_collection_from_object_storage
+
+logger = get_logger(__name__)
 
 def compute_extra(args):
-    load_scanr_publications('/upw_data/scanr/publications_denormalized.jsonl', 'scanr-publications-20230912')
+    #myclient = pymongo.MongoClient('mongodb://mongo:27017/')
+    #mydb = myclient['unpaywall']
+    #collection_name = 'global'
+    #mycol = mydb[collection_name]
+    #logger.debug('creating index')
+    #mycol.create_index('journal_issn_l')
+    #logger.debug('done')
+    #myclient.close()
+
+    asof = args.get('asof', 'nodate')  # if nodate, today's snapshot will be used
+    #filename = download_snapshot(asof).split('/')[-1]
+    #logger.debug(f'Filename after download is {filename}')
+    filename='unpaywall_snapshot_2023-10-22T083001.jsonl.gz'
+    path = f'/upw_data/{filename}'
+    jq_oa = f'zcat {path} | '
+    jq_oa += "jq -r -c '{journal_issn_l}' | sort -u > /upw_data/issn_l"
+    logger.debug(jq_oa)
+    os.system(jq_oa)
+    #logger.debug(f'Deleting file {path}')
+    #os.remove(path)
+
+
+
+    #load_scanr_publications('/upw_data/scanr/publications_denormalized.jsonl', 'scanr-publications-20230912')
+    #load_scanr_publications('/upw_data/scanr/persons.jsonl', 'scanr-persons-20230912')
 
 def compute_extra2(args):
     to_download = False
