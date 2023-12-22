96774ad112baf21c75ae58ed36db34f5f8a8a798
eric <eric.jeangirard@recherche.gouv.fr>
2023-12-22 15:52:05 +0100

    misc

--- bso/server/main/extra_treatment.py
+++ bso/server/main/extra_treatment.py
@@ -3,15 +3,62 @@ import os
 import requests
 import pymongo
 from bso.server.main.logger import get_logger
-from bso.server.main.utils_swift import download_object, upload_object
+from bso.server.main.utils_swift import download_object, upload_object, delete_object, delete_objects
 from bso.server.main.bso_utils import get_ror_from_local
 from bso.server.main.utils import to_jsonl
 from bso.server.main.extract_transform import load_scanr_publications
 from bso.server.main.unpaywall_feed import download_daily, download_snapshot, snapshot_to_mongo, load_collection_from_object_storage
+from bso.server.main.scanr import clean_sudoc_extra, get_person_ids
+import hashlib
 
 logger = get_logger(__name__)
 
+
+def get_hash(x):
+    return hashlib.md5(x.encode('utf-8')).hexdigest()
+
+
+def get_filename(doi):
+    init = doi.split('/')[0]
+    notice_id = f'doi{doi}'
+    id_hash = get_hash(notice_id)
+    filename = f'{init}/{id_hash}.json.gz'
+    return filename
+
+def tmp(args):
+    dois_to_clean = []
+    df = pd.read_json('https://storage.gra.cloud.ovh.net/v1/AUTH_32c5d10cb0fe4519b957064a111717e3/bso_dump/bso-publications-latest_split_2021_enriched.jsonl.gz',
+            chunksize=5000, lines=True, orient='records')
+    for c in df:
+        publications = c.to_dict(orient='records')
+        for p in publications:
+            if isinstance(p.get('bso_country'), list) and isinstance(p.get('bso_country_corrected'), list):
+                if 'fr' in p['bso_country'] and ('fr' not in p['bso_country_corrected']) and 'html' in p['sources']:
+                    for x in p['all_ids']:
+                        if x[0:3]=='doi':
+                            dois_to_clean.append(x[3:])
+    print(len(dois_to_clean))
+
 def compute_extra(args):
+
+    index_name = args.get('index')
+    logger.debug(f'reading {index_name}')
+    df = pd.read_json(f'/upw_data/{index_name}.jsonl', lines=True, chunksize=1000)
+    ix = 0
+    for c in df:
+        publications = c.to_dict(orient='records')
+        publications = [p for p in publications if 'sudoc' in p['id']]
+        logger.debug(f'{len(publications)} to clean {ix} for {index_name}')
+        publications = get_person_ids(publications)
+        sudocs_to_rm = []
+        for p in publications:
+            sudoc_to_rm = clean_sudoc_extra(p)
+            if sudoc_to_rm:
+                sudocs_to_rm.append(sudoc_to_rm)
+                os.system(f'rm -rf /upw_data/sudoc/{sudoc_to_rm}')
+        #delete_objects('sudoc', sudocs_to_rm)
+        ix += 1
+
     #myclient = pymongo.MongoClient('mongodb://mongo:27017/')
     #mydb = myclient['unpaywall']
     #collection_name = 'global'
@@ -21,15 +68,15 @@ def compute_extra(args):
     #logger.debug('done')
     #myclient.close()
 
-    asof = args.get('asof', 'nodate')  # if nodate, today's snapshot will be used
+    #asof = args.get('asof', 'nodate')  # if nodate, today's snapshot will be used
     #filename = download_snapshot(asof).split('/')[-1]
     #logger.debug(f'Filename after download is {filename}')
-    filename='unpaywall_snapshot_2023-10-22T083001.jsonl.gz'
-    path = f'/upw_data/{filename}'
-    jq_oa = f'zcat {path} | '
-    jq_oa += "jq -r -c '{journal_issn_l}' | sort -u > /upw_data/issn_l"
-    logger.debug(jq_oa)
-    os.system(jq_oa)
+    #filename='unpaywall_snapshot_2023-10-22T083001.jsonl.gz'
+    #path = f'/upw_data/{filename}'
+    #jq_oa = f'zcat {path} | '
+    #jq_oa += "jq -r -c '{journal_issn_l}' | sort -u > /upw_data/issn_l"
+    #logger.debug(jq_oa)
+    #os.system(jq_oa)
     #logger.debug(f'Deleting file {path}')
     #os.remove(path)
 
