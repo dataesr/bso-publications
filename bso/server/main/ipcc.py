ccc6c813c3ac614f1c733dcb17dc90486f34189c
victor barbier <victor.barbier@recherche.gouv.fr>
2024-10-29 10:01:56 +0100

    [release] version 0.0.41

--- bso/server/main/ipcc.py
+++ bso/server/main/ipcc.py
@@ -1,12 +1,30 @@
 from bso.server.main.logger import get_logger
 import fasttext
+import os
 
 logger = get_logger(__name__)
 
-model_teds = fasttext.load_model("fasttext/fasttext_model_teds.bin")
+
+model_path = "/src/bso/server/main/fasttext/fasttext_model_teds.bin"
+
+
+def fasttext_load():
+    try:
+        return fasttext.load_model(model_path)
+    except Exception as error:
+        logger.error(error)
+        return None
+
+
+model_teds = fasttext_load()
 
 
 def add_predict_ipcc(publications):
+
+    if model_teds is None:
+        logger.warn("predict_ipcc model not loaded: abort prediction")
+        return publications
+
     logger.debug("predict_ipcc_with_fasttext")
 
     for p in publications:
